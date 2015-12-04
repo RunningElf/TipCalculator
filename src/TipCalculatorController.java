eb33daaaf5651aeedcc775942080cd21c878630e
Marianne <nikkita.hirayama@gmail.com>
2015-12-04 12:14:42 -0500

    Calculates the tip and total bill
    Input a cost and the tip amount you want using the slider. The
    calculator will tell you how much to tip and the total cost of the bill.

---
+++ src/TipCalculatorController.java
@@ -0,0 +1,121 @@
+// TipCalculatorController.java
+// Controller that handles calculateButton and tipPercentageSlider events
+
+/*
+
+*/
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class TipCalculatorController 
+{
+   // formatters for currency and percentages
+   private static final NumberFormat currency = 
+      NumberFormat.getCurrencyInstance();
+   private static final NumberFormat percent = 
+      NumberFormat.getPercentInstance();
+   
+   private BigDecimal tipPercentage = new BigDecimal(0.15); // 15% default
+   
+   // GUI controls defined in FXML and used by the controller's code
+   @FXML 
+   private TextField amountTextField; 
+
+   @FXML
+   private Label tipPercentageLabel; 
+
+   @FXML
+   private Slider tipPercentageSlider;
+
+   @FXML
+   private TextField tipTextField;
+
+   @FXML
+   private TextField totalTextField;
+
+   // calculates and displays the tip and total amounts
+  
+   // called by FXMLLoader to initialize the controller
+   public void initialize() 
+   {
+      // 0-4 rounds down, 5-9 rounds up 
+      currency.setRoundingMode(RoundingMode.HALF_UP);
+      
+      
+      //binds percentageLabel with slider
+      tipPercentageLabel.textProperty().bind(tipPercentageSlider.valueProperty().asString("%.0f%%"));
+      // listener for changes to tipPercentageSlider's value
+      
+     tipPercentageSlider.valueProperty().addListener(
+         new ChangeListener<Number>() 
+         {
+            @Override
+            public void changed(ObservableValue<? extends Number> ov, 
+               Number oldValue, Number newValue) 
+            {
+               tipPercentage = 
+                  BigDecimal.valueOf(newValue.intValue() / 100.0);//sets tipPercentage
+               //tipPercentageLabel.setText(percent.format(tipPercentage));//sets the percentage
+               
+              calculateTip();//calculates the tip for the user
+         
+            }
+         }
+      );
+      
+      //listener for changes to amountTextField's values
+      amountTextField.textProperty().addListener(new ChangeListener<String>() {
+    @Override
+    public void changed(ObservableValue<? extends String> observable,
+            String oldValue, String newValue) {
+
+        calculateTip();//calculate tip
+    }
+});
+      
+      
+   }
+
+
+private void calculateTip()
+{
+try 
+      {
+         BigDecimal amount = new BigDecimal(amountTextField.getText());//gets the amount from the user input
+         BigDecimal tip = amount.multiply(tipPercentage);//multipies it by percentage
+         BigDecimal total = amount.add(tip);//adds the user input to the tip percentage
+
+         tipTextField.setText(currency.format(tip));//displayes tip
+         totalTextField.setText(currency.format(total));//displays total
+      }
+      catch (NumberFormatException ex)//if there is nothing in the amountTextField set fields to empty
+      {
+         tipTextField.setText("");
+         totalTextField.setText("");
+      }
+   }
+
+
+}
+/**************************************************************************
+ * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
